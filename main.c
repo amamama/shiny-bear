9f7aa19ae1236d14e6efb3b5d4ac677c450a60ea
amamama <amtkrogzo@yahoo.co.jp>
2014-10-05 14:45:32 +0900

    add jansson sample

--- main.c
+++ main.c
@@ -2,17 +2,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #include <oauth.h>
 #include <curl/curl.h>
 #include <ncurses.h>
+#include "jansson.h"
 
 #include "include/tweet.h"
 #include "keys.h"
 
 int init(void) {
-	bear_init(&(union KEYS){.keys_array = {c_key, c_sec, t_key, t_sec}});
+	bear_init(c_key, c_sec, t_key, t_sec);
 
 	initscr();
 	cbreak();
@@ -29,11 +31,43 @@ int fini(void) {
 	return 0;
 }
 
-int main(void)
-{
+int main(void) {
+
 	init();
-	box(stdscr, 0, 0);
+	border(0x7c, 0x7c, 0x2d ,0x2d, 0x2b, 0x2b ,0x2b, 0x2b);
+	char *res = NULL;
+	int ret = get_statuses_user_timeline(&res, 0, "sadi_gear", 0, 0, 0, -1, -1, -1, -1);
+	//puts(res);
+	mvaddch(1,1,ret + 0x30);
+
+	json_error_t json_err;
+	json_t *result = json_loads(res, 0, &json_err);
+
+	json_t *tweet;
+	json_array_foreach(result, i, tweet) {
+		char const *key;
+		json_t *value;
+		json_object_foreach(tweet, key, value) {
+			switch(json_typeof(value)) {
+				case JSON_NULL:
+				case JSON_FALSE:
+				case JSON_TRUE:
+				break;
+
+				case JSON_STRING:
+				printw("%s:%s\n", key, json_string_value(value));
+				break;
+
+				default:
+				printw("%s:%s\n",key,((char const*[JSON_NULL + 1]){"json_object", "json_array", "json_string", "json_integer", "json_real", "json_true", "json_false", "json_null"})[json_typeof(value)]);
+				//break;
+			}
+		}
+		break;
+	}
+
 	getch();
+	json_decref(result);
 	fini();
 	return 0;
 }
